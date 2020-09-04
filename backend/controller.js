2c227dca9cab365796130ef46bbdababbd40f31e
berliners1 <steven@stevenberliner.me>
2020-09-03 20:30:10 -0500

    Add validation for and escape route parameters

--- backend/controller.js
+++ backend/controller.js
@@ -1,9 +1,19 @@
+const { validationResult } = require('express-validator');
 require('dotenv').config();
 
 const apikey = process.env.API_KEY;
 
 const getMoviesList = (req, res, next) => {
     const {title, page} = req.params;
+
+    //error if express-validator conditions are not met.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            success: false,
+            errors: errors.array()
+        });
+    }
 
     //build full url
     const url = `https://www.omdbapi.com/?s=${title}&page=${page}&type=movie&r=json&apikey=${apikey}`;
@@ -13,6 +23,15 @@ const getMoviesList = (req, res, next) => {
 
 const getMovieDetails = async(req, res, next) => {
     const title = req.params.title;
+
+    //error if express-validator conditions are not met.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            success: false,
+            errors: errors.array()
+        });
+    }
 
     //build full url
     const url = `https://www.omdbapi.com?t=${title}&r=json&apikey=${apikey}`;
--- backend/routes.js
+++ backend/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const { param } = require('express-validator');
 
 const controller = require('./controller');
 const router = express.Router();
 
 //example: https://website.com/api/s=batman&page=1
-router.get('/s=:title&page=:page', controller.getMoviesList);
+router.get('/s=:title&page=:page', [
+    param('title').isLength({min: 1}).trim().escape(),
+    param('page').isLength({min: 1}).isNumeric().trim().escape()
+], controller.getMoviesList);
 
 //example: https://website.com/api/t=batman
-router.get('/t=:title', controller.getMovieDetails);
+router.get('/t=:title', [
+    param('title').isLength({min: 1}).trim().escape()
+], controller.getMovieDetails);
 
 module.exports = router;
