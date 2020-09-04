ff61e6898799898e2a176e753f2638c07cda11e6
berliners1 <steven@stevenberliner.me>
2020-09-03 23:18:49 -0500

    Implement basic fetch on api in MoviesList with imported parameters

--- frontend/react-movie-search/src/components/MoviesList.js
+++ frontend/react-movie-search/src/components/MoviesList.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function MoviesList(props) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    //will fetch list of movies
-    return( 
-    <>
-        {alert("boo!")}
-        <p>{props.title}</p>
-        <p>{props.pagenum}</p>
-    </>
+  let title = props.title;
+  let page = props.pagenum;
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/s=${title}&page=${page}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.Search);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.Title}>
+            {item.Title}
+          </li>
+        ))}
+      </ul>
     );
+  }
+
 
 }
 
