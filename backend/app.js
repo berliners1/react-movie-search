45c157af0f53e3d23b0ad06546cf2b60c01c7d8b
berliners1 <steven@stevenberliner.me>
2020-09-03 22:49:07 -0500

    Adjust cors headers to allow localhost 3000

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,19 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        'http://localhost:3000');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE');
+    next();
+});
+
 //set up routes
 app.use('/api', routes);
 
