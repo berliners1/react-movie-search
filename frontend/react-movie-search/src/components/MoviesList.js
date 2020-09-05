5a31ea9e3a8e928dc604e140c24305c8f6a215e5
berliners1 <steven@stevenberliner.me>
2020-09-04 21:51:33 -0500

    Add some error handling for bad query inputs in MoviesList results

--- frontend/react-movie-search/src/components/MoviesList.js
+++ frontend/react-movie-search/src/components/MoviesList.js
@@ -1,46 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-  function MoviesList(props) {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+function MoviesList(props) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-      fetch(`http://localhost:5000/api/s=${props.title}&page=${props.pagenum}`)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result.Search);
-            console.log('api call run');
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [props.title, props.pagenum])
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/s=${props.title}&page=${props.pagenum}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setError(null);
+          setIsLoaded(true);
+          setItems(result.Search);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [props.title, props.pagenum])
 
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.Title}>
-              {item.Title}
-            </li>
-          ))}
-        </ul>
-      );
-    }
-
+  if (error) {
+    return <div>Error - could not search with these queries.</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else if (items === undefined){
+    return <p>Could not find anything.</p>
+  } else {
+    return (
+      <>
+      <ul>
+        {items.map(item => (
+          <li key={item.imdbID}>
+            {item.Title}
+          </li>
+        ))}
+      </ul>
+      </>
+    );
+  }
 
 }
 
