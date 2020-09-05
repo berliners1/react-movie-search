6caf3c38d26da097390e1e78889abcba2da3aa2d
berliners1 <steven@stevenberliner.me>
2020-09-04 19:50:32 -0500

    Make MoviesList useEffect refresh when title or pagenum updates from parrent App.js props

--- frontend/react-movie-search/src/components/MoviesList.js
+++ frontend/react-movie-search/src/components/MoviesList.js
@@ -1,46 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function MoviesList(props) {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  function MoviesList(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-  let title = props.title;
-  let page = props.pagenum;
+    useEffect(() => {
+      fetch(`http://localhost:5000/api/s=${props.title}&page=${props.pagenum}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result.Search);
+            console.log('api call run');
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [props.title, props.pagenum])
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/api/s=${title}&page=${page}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result.Search);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.Title}>
-            {item.Title}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.Title}>
+              {item.Title}
+            </li>
+          ))}
+        </ul>
+      );
+    }
 
 
 }
