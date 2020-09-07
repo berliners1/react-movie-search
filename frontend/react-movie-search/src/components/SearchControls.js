0d5de803a27a93f8fece5501fa904cdce40d4022
berliners1 <steven@stevenberliner.me>
2020-09-06 22:01:45 -0500

    Do search automatically if both needed parameters already exist in entered url

--- frontend/react-movie-search/src/components/SearchControls.js
+++ frontend/react-movie-search/src/components/SearchControls.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory , useParams} from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 function SearchControls(props) {
     const [title, setTitle] = useState('');
     const history = useHistory();
     const {t,p} = useParams();
 
-
     useEffect(() => {
+        if(t && p){
+            console.log('both t and p have inputs');
+            props.childToParent(t);
+        }
+
         //if title exists in url parameters, and if input is empty
         if(t && title === ""){
             console.log('t exists and form is empty');
